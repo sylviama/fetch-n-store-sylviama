2294ea46c3b6afe411d80f1591a5625eeae4a094
Sylvia Ma <xiangwma@umich.edu>
2016-10-22 13:08:30 -0500

    init

---
+++ fetchNStore/Scripts/app/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("fetchNStore", []);
+
+app.controller("fetchButton", function ($scope) {
+    $scope.fetchValue = "wakakakaka";
+    $scope.methods = ["Get", "Post", "Put", "Delete"];
+    console.log($scope.methodSelected);
+    $scope.fetchFunction=function()
+    {
+        console.log("this is the result after clicking ng-click");
+    }
+    
+})
